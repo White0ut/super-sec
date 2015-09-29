b643abac4cd449265fa070f91ec2f4e655d3c11c
Kendrick Cline <kdecline@gmail.com>
2015-09-29 19:55:04 -0400

    updating to project specs

---
+++ project1/src/main/get/get.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "unistd.h"
+
+/**
+* Silent exists emit "silent exit" to the terminal before exiting
+* 
+* Usage: get <source> <destination>
+*/
+int main(int argc, char** argv) {
+	printf("We are working here\n");
+
+	/* If the ACL file does not exist, exit silently */
+
+	/* malformed ACL entry causes a silent exit (FLEX file ?? ) */
+
+	/* if the ACL is a sym link, exit silently */
+
+	/* if the protection for the ACL allows world or group access exit silently */
+
+	/* if the file is not an ordinary file, exit silently */
+
+	// Get the Effective UID geteuid()
+	// Get the Real UID getuid()
+
+	
+}
