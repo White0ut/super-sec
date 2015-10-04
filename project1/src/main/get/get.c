ae50eee78722e085d24e10b3990c4bb3864c9d92
Kendrick Cline <skylynx@Kendricks-MacBook-Pro.local>
2015-10-04 14:07:49 -0500

    added more to get

--- project1/src/main/get/get.c
+++ project1/src/main/get/get.c
@@ -6,7 +6,7 @@
 
 #define DEBUG 1
 
-int is_sym_lnk(char* path);
+int csyparse();
 
 /**
 * Silent exists emit "silent exit" to the terminal before exiting
@@ -26,17 +26,39 @@ int main(int argc, char** argv) {
     FILE *acl_fp;
     FILE *destination_fp;
     char *acl_path = strncat(argv[1], ".access", 7);
-   
+
     /* if the ACL file is a sym link, exit silently */
-    if (is_sym_lnk(acl_path)) {
+    /* TODO: if the protection for the ACL allows world or group access exit silently */
+    struct stat buf;
+	int x;
+
+	x = lstat(path, &buf);
+	if (x < 0) {
+		printf("silent exit\n");
+		if (DEBUG) {
+			printf("Can't read file info\n");
+		}
+		return 1;
+	}
+
+	if (S_ISLNK(buf.st_mode)) {
+		printf("silent exit\n");
+		if (DEBUG) {
+			printf("ACL is a sym link\n");
+		}
+		return 1;
+	}
+
+    source_fp  = fopen(argv[1], "r");
+
+    if (source_fp == NULL) {
     	printf("silent exit\n");
     	if (DEBUG) {
-    		printf("ACL is a sym link\n");
+			printf("Source file doesn't exist\n");
     	}
     	return 1;
     }
 
-    source_fp  = fopen(argv[1], "r");
     acl_fp = fopen(acl_path, "r");
 
 	/* If the ACL file does not exist, exit silently */
@@ -48,15 +70,33 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-	destination_fp = fopen(argv[2], "w");
+	destination_fp = fopen(argv[2], "w+");
+
+	if (destination_fp == NULL) {
+		printf("silent exit\n");
+		if (DEBUG) {
+			printf("Destination file doesn't exist\n");
+		}
+		return 1;
+	}
 
 	/* malformed ACL entry causes a silent exit (FLEX file ?? ) */
-	
-	/* if the protection for the ACL allows world or group access exit silently */
+	if (casyparse()) {
+		printf("silent exit\n");
+		if (DEBUG) {
+			printf("malformed ACL entry\n");
+		}
+		return 1;
+	}
 
 	/* if the file is not an ordinary file, exit silently */
 
 	// Get the Effective UID geteuid()
+	uid_t euid = geteuid();
+
+	uid_t ruid = getuid();
+
+	printf("euid:%u\nruid:%u\n", euid, ruid);
 	// Get the Real UID getuid()
 
 	fclose(source_fp);
@@ -65,17 +105,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-/**
-* if the file point to by *path* is a sym link, return 1
-* otherwise return 0
-*/
-int is_sym_lnk(char *path) {
-	struct stat buf;
-	int x;
-
-	x = lstat(path, &buf);
-	if (x < 0) return 0;
-
-	if (S_ISLNK(buf.st_mode)) return 1;
-	else return 0;
+int csyparse() {
+	return 0;
 }
+
